23ba6514b4301f34a1bd0391ac75e25af850aa2b
Onur <onur.yilmazer1@gmail.com>
2018-03-09 00:39:35 +0300

    remove unnecessary code

--- main.js
+++ main.js
@@ -119,46 +119,6 @@ function getPlayerSelection(domNode) {
     { throw "player input is not recognized!"; }
 }
 
-
-function gameOld() {
-    let rounds = Number(prompt("How many rounds will you play?", "3"));
-    while (!rounds) {
-        rounds = Number(prompt("Enter a number.", "3"));
-    }
-
-    let playerScore = 0;
-
-    for (let i = 0; i < rounds; i++) {
-        let playerSelection = prompt("Rock, paper, or scissors?","");
-        while (!checkPlayerInput(playerSelection)) {
-            playerSelection = prompt("Invalid selection. Please pick either rock, paper or scissors.","");
-        }
-
-        let computerSelection = playComputer();
-
-        let result = playRound(playerSelection, computerSelection);
-        if (result == "won") {
-            playerScore++;
-        }
-        else if (result == "lost") {
-            playerScore--;
-        }
-        else if (result == "tie") {
-
-        }
-        else {
-            alert("Something bad happened. " + result);
-            i--;
-            continue;
-        }
-
-        alert(playerSelection + " " + result + " against " + computerSelection + "!\n Score:" + playerScore);
-
-    }
-
-    alert("Your score is " + playerScore + "! You " + (playerScore > 0 ? "won!" : playerScore == 0 ? "are tied with the computer!" : "lost.") );
-}
-
 function checkPlayerInput(input) {
     let validInputs = ["rock", "paper", "scissors"];
     return input !== null && validInputs.indexOf(input.toLowerCase().trim()) != -1 ? true : false;
